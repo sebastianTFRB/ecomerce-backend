22e38edc978c4801a80db68e782a83cfc80f2043
sebastianTFRB <sebastian.fajardo@campusucc.edu.co>
2024-05-23 19:34:05 -0500

    subir

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',"comerceb.azurewebsites.net"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',"ecomerback.azurewebsites.net"]
 
 
 # Application definition
